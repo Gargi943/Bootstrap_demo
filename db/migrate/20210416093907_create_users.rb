f3199d93547759f2959ffe909fe9483b63758343
Gargi943 <gargiyuvasoft291@gmail.com>
2021-04-19 19:02:14 +0530

    commit-19-apr

---
+++ db/migrate/20210416093907_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone_number
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :pincode
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
